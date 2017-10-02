b4af27d1998a0e2437eb54de91b07c5e3045ffa4
ansh233432545 <32316626+ansh233432545@users.noreply.github.com>
2017-10-02 16:31:37 -0500

    Add files via upload

---
+++ console graphics engine/cgecore.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cosmos.Common;
+using Cosmos.Core;
+using Cosmos.Debug;
+using Cosmos.HAL;
+
+
+namespace console_graphics_engine
+{
+    
+    public class cgecore { 
+    
+        public void bootend() {
+            
+            Console.Clear();
+            Console.WriteLine("////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("////                   /////////////////////////////////////////////////");
+            Console.WriteLine("////                   /////////////////////////////////////////////////");
+            Console.WriteLine("//////////     ////////////// TACHYON OS 2.0 ///////////////////////////");
+            Console.WriteLine("//////////     ///////////// BUILD 42 REV 8 DEV ////////////////////////");
+            Console.WriteLine("//////////     /////////////////////////////////////////////////////////");
+            Console.WriteLine("//////////     /////////////////////////////////////////////////////////");
+            Console.WriteLine("//////////     /////////////////////////////////////////////////////////");
+            Console.WriteLine("//////////     /////////////////////////////////////////////////////////");
+            Console.WriteLine("//////////     /////////////////////////////////////////////////////////");
+            Console.WriteLine("////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("///// COPYRIGHT 2017 THE TACHYON PROJECT ///////////////////////////////");
+            Console.WriteLine("////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("////// DEV BUILD- 50.0 /////////////////////////////////////////////////");
+            Console.WriteLine("////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("////////////////////////////////////////////////////////////////////////");
+
+        }
+
+        public void BOOT() {
+            Console.Clear();
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                        ///////////////                                         ");
+            Console.WriteLine("                            //////                                              ");
+            Console.WriteLine("                            /////                                              ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////
+            Console.Clear();
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                        ///////////////                                         ");
+            Console.WriteLine("                            //////                                              ");
+            Console.WriteLine("                            /////                                              ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                       Ta                                                       ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////
+            Console.Clear();
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                        ///////////////                                         ");
+            Console.WriteLine("                            //////                                              ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                       Tach                                                     ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////
+            Console.Clear();
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                        ///////////////                                         ");
+            Console.WriteLine("                            //////                                              ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                            /////                                               ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                       Tachyon os 2.0                                           ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            Console.WriteLine("                                                                                ");
+            bootend();
+
+        }
+        public static void animation() {
+            Console.WriteLine("                                    ///                                         ");
+        }
+
+
+        
+    }
+    ////////////////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
